ebdc3512217761ea986e9adc9accf10b9ccace11
Yaasir Mahamadou-Bachirou <Yaasir.Mahamadou@segra.com>
2022-04-26 19:20:50 -0400

    Added to and from file functions and cleaned up some code. Working now.

--- final_project.py
+++ final_project.py
@@ -13,12 +13,16 @@ from bs4 import BeautifulSoup
 
 
 def book_search(isbn13):
-    # Spoofing the user agent.
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'}
-    # This site only works with ISBN-13s for some reason.
-    response = requests.get(f"https://www.isbnsearcher.com/books/{isbn13}", headers=headers)
+    """ Function that searches an ISBN number online. """
+    try:
+        # Spoofing the user agent.
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36'}
+        # This site only works with ISBN-13s for some reason.
+        response = requests.get(f"https://www.isbnsearcher.com/books/{isbn13}", headers=headers)
+    except:
+        print('An error has occured while trying to search your book online.')
 
-    print(response.status_code)
+    #print(response.status_code)
     soup = BeautifulSoup(response.text, 'lxml')
     book_title = soup.find('h1')
     title = book_title.text
@@ -26,20 +30,27 @@ def book_search(isbn13):
     # print(title.group(1))
     # List containing all "col-8 col-sm-9 mb-1" classes
     book_info = soup.find_all(class_='col-8')
+    print(book_title)
+    print(book_info)
 
-    language = book_info[2].text
-    category = book_info[3].text
-    page_count = book_info[4].text
-    publish_date = book_info[5].text
-    author = book_info[6].text
-    
-    print("Here's some information about your book :) \n ")
-    print('Title:', title)
-    print('Language:', language)
-    print('Category:', category)
-    print('Page count:', page_count)
-    print('Published:', publish_date)
-    print('Author', author)
+    if response.status_code == 200:
+
+        language = book_info[2].text
+        category = book_info[3].text
+        page_count = book_info[4].text
+        publish_date = book_info[5].text
+        author = book_info[6].text
+        
+        print("Here's some information about your book :) \n ")
+        print('Title:', title)
+        print('Language:', language)
+        print('Category:', category)
+        print('Page count:', page_count)
+        print('Published:', publish_date)
+        print('Author:', author)
+    else:
+        print('An error has occured while trying to search your book online.')
+
 
 def isbn_13_chk_dgt(isbn_num_13):
     """ Function that calculates the check digit for a 13 digit ISBN number."""
@@ -126,17 +137,17 @@ def isbn_10_to_13(isbn_num_10):
 def isbn_13_to_10(isbn_num_13):
     """ Function that converts isbn 13 number to isbn 10. """
     num_list = list(isbn_num_13)
-    
     pad = ['9', '7', '8']
-    # Make sure this is a valid isbn 13 number.
-    if num_list[:3] == pad:
-        for num in pad:
-            num_list.remove(num)
 
     if len(num_list) == 13:
         # Remove the check digit from the isbn 13 
         # so the new one can get calculated
         num_list.pop(-1)
+
+    # Make sure this is a valid isbn 13 number.
+    if num_list[:3] == pad:
+        for num in pad:
+            num_list.remove(num)
 
     check_digit = isbn_10_chk_dgt(num_list)
     num_list.append(str(check_digit))
@@ -148,34 +159,71 @@ def isbn_13_to_10(isbn_num_13):
 testnum13 = '9781861972712'
 testnum10 = '1861972717'
 
-#print(isbn_10_to_13(testnum10))
+def isbn_to_file(isbn, chk_dgt):
+    """ Function that writes ISBN numbers and their check digits to a file. """
+    with open('ISBN_number.txt', 'a') as f:
+        f.write(f'The check digit of the ISBN number {isbn} is: {chk_dgt} \n')
+    print('Your ISBN number has been written to a file.')
 
-
-
-
-# Not fully working
+def isbn_from_file(path):
+    """ Function that reads ISBN numbers from a file and calculates the check digit. """
+    with open(path, 'r') as txt:
+        for line in txt:
+            if len(line) == 13 or len(line) == 12:
+                #isbn_13_chk_dgt(line)
+                print(line)
+                print(f'The check digit of the ISBN number {line} is: {isbn_13_chk_dgt(line)}')
+            elif len(line) == 10 or len(line) == 9:
+                print(type(line))
+                #isbn_10_chk_dgt(line)
+                print(f'The check digit of the ISBN number {line} is: {isbn_10_chk_dgt(line)}')
+        
+# Main menu       
 while True:
-
+    
     print("""
     1. Verify the check digit of an ISBN-10
     2. Verify the check digit of an ISBN-13
     3. Convert an ISBN-10 to an ISBN-13
     4. Convert an ISBN-13 to an ISBN-10
-    5. Exit \n """)
+    5. Online search 
+    6. Save ISBN number to file
+    7. Read ISBN number from file
+    8. Exit \n """)
 
     user = input("Please select an option by entering the number of the menu item you'd like to use: ")
 
     if user == '1':
         isbn_num = input('Enter your ISBN-10 number without the dashes: ')
-        print('Your check digit is ',  isbn_10_chk_dgt(isbn_num))
+        print('Your check digit is ',  isbn_10_chk_dgt(isbn_num))        
+
     elif user == '2':
         isbn_num = input('Enter your ISBN-13 number without the dashes: ')
         print('Your check digit is ', isbn_13_chk_dgt(isbn_num))
+
     elif user == '3':
         isbn_num = input('Enter your ISBN-10 number without the dashes: ')
+
         print('Your ISBN 13 number is ', isbn_10_to_13(isbn_num))
     elif user == '4':
         isbn_num = input('Enter your ISBN-13 number without the dashes: ')
         print('Your ISBN 10 number is ', isbn_13_to_10(isbn_num))
+
     elif user == '5':
+        isbn_num = input('Enter the ISBN-13 number without the dashes of the book you would like to search: ')
+        book_search(isbn_num)
+
+    elif user == '6':
+        isbn_num = input('Enter your ISBN number without the dashes: ')
+        if len(isbn_num) == 13 or len(isbn_num) == 12:
+            isbn_to_file(isbn_num, isbn_13_chk_dgt(isbn_num))
+        elif len(isbn_num) == 10 or len(isbn_num) == 9:
+            isbn_to_file(isbn_num, isbn_10_chk_dgt(isbn_num))
+
+    elif user =='7':
+        path_to_file = input('Please enter the path to the file: ')
+        isbn_from_file('ISBN_number.txt')
+
+    elif user == '8':
+        print('Good Bye :)')
         break
